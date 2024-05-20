0a5e776fe35e6004b52667f25a31db7fdaee0e85
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-05-20 10:35:29 +0200

    Update sql_practice.sql

--- sql_practice.sql
+++ sql_practice.sql
@@ -1,32 +1,3 @@
-// SQLBOLT RESULTS
-# EXERCISE 1
-// Find the title of each film
-SELECT * FROM movies
-// find te director of each film
-SELECT director FROM movies
-// Find the title and director of each film
-SELECT title, director FROM movies
-// Find the title and year of each film
-SELECT title, year FROM movies
-// Find all the information about the film
-SELECT * FROM movies
-
-
-#EXERCISE 2
-//Find the movie with a row id of 6
-SELECT * FROM movies
-WHERE id = 6
-// find the movies released in the years between 2000 and 2010
-SELECT * FROM movies
-WHERE year BETWEEN 2000 AND 2010
-// Find the movies not released in the year between 2000 and 2010
-SELECT * FROM movies
-WHERE year NOT BETWEEN 2000 AND 2010
-// Find the first 5 Pixar movies and their release year
-SELECT title, year FROM movies
-WHERE year <= 2003;
-
-
 #EXERCISE 3
 //Find all the Toy Story movies
 SELECT * FROM movies
